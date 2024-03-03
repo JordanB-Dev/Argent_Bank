603104363054e8a7f1c15313a644bb2a557f8574
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-03 18:17:25 +0100

    Style

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+import './assets/styles/style.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
