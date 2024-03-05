51392757fd7f6e93dbf02358d5bb01fa73cdfdaf
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-05 12:41:38 +0100

    Redux store

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,24 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { auth } from './redux/auth'
 import App from './components/App'
 import './assets/styles/style.css'
 
+const state = {}
+
+const store = configureStore({
+  preloadedState: state,
+  reducer: combineReducers({
+    auth: auth.reducer,
+  }),
+})
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 )
