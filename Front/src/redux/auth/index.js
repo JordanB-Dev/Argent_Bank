975ee150785bebef89c00a1668202ecc0f138be3
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-06 17:29:05 +0100

    Redux Action/Reducer Auth

--- Front/src/redux/auth/index.js
+++ Front/src/redux/auth/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+/* eslint-disable no-undef */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
   credentials: '',
@@ -7,6 +8,28 @@ const initialState = {
   status: 'idle',
   error: null,
 }
+
+export const getCredentials = createAsyncThunk(
+  'auth/getCredentials',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        `${process.env.URL}/api/v1/user/login`,
+        credentials,
+        {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          header: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.code)
+    }
+  },
+)
 
 export const auth = createSlice({
   name: 'auth',
@@ -22,5 +45,11 @@ export const auth = createSlice({
       state.status = 'idle'
       state.error = null
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(getCredentials.pending, (state) => {
+      state.status = 'loading'
+      state.isAuth = false
+    })
   },
 })
