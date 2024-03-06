8121a5b3e3d99e117c087119b94956f42cdd88fe
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-06 16:37:43 +0100

    Redux

--- Front/src/redux/auth/index.js
+++ Front/src/redux/auth/index.js
@@ -1,1 +1,14 @@
-const initialState = {}
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  isAuth: false,
+  token: null,
+  status: 'idle',
+  error: null,
+}
+
+export const auth = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+})
