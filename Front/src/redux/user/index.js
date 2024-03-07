ce362270d8a3d4f365cd2ffe8205df07d79c1afa
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-07 16:46:04 +0100

    Redux user

--- Front/src/redux/user/index.js
+++ Front/src/redux/user/index.js
@@ -12,8 +12,14 @@ const initialState = {
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    resetUserState: (state) => Object.assign(state, initialState),
+  },
   extraReducers: (builder) => {
     builder
   },
 })
+
+export const { resetUserState } = userSlice.actions
+
+export const userReducer = userSlice.reducer
