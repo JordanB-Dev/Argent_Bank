7b382d879960f25d991858d67791d18a6c97d2e0
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-07 17:20:48 +0100

    Router & Page/Error

--- Front/src/components/App/index.jsx
+++ Front/src/components/App/index.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../../pages/Home'
 import Login from '../../pages/Login'
 import Profile from '../../pages/Profile'
-import ErrorPage from '../../pages/ErrorPage'
+import Error from '../../pages/Error'
 import Header from '../../layout/Header'
 import Footer from '../../layout/Footer'
 
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </>
