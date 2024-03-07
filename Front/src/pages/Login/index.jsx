ecc8fbc759ec7cb156b0ef8ca34649bbfd7744d8
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-07 23:20:45 +0100

    Components From login

--- Front/src/pages/Login/index.jsx
+++ Front/src/pages/Login/index.jsx
@@ -1,5 +1,15 @@
+import LoginFrom from '../../components/LoginFrom'
+
 const Login = () => {
-  return <h1>Login</h1>
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <LoginFrom />
+      </section>
+    </main>
+  )
 }
 
 export default Login
