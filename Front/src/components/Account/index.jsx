be75875e461f6aef9bfbd5828d7449b47dd01645
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-11 01:52:11 +0100

    Home/Account

--- Front/src/components/Account/index.jsx
+++ Front/src/components/Account/index.jsx
@@ -1,5 +1,18 @@
-const Account = () => {
-  return <></>
+/* eslint-disable react/prop-types */
+const Account = (props) => {
+  const { title, price, desc } = props
+  return (
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title"> {title} </h3>
+        <p className="account-amount"> {price} </p>
+        <p className="account-amount-description"> {desc} </p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
+  )
 }
 
 export default Account
--- Front/src/pages/Profile/index.jsx
+++ Front/src/pages/Profile/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserState } from '../../redux/user'
+import Account from '../../components/Account'
 
 const Profile = () => {
   const dispatch = useDispatch()
@@ -22,7 +23,21 @@ const Profile = () => {
         </h1>
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <section className="account"></section>
+      <Account
+        title="Argent Bank Checking (x8349)"
+        price="$2,082.79"
+        desc="Available Balance"
+      />
+      <Account
+        title="Argent Bank Savings (x6712)"
+        price="$10,928.42"
+        desc="Available Balance"
+      />
+      <Account
+        title="Argent Bank Credit Card (x8349)"
+        price="$184.30"
+        desc="Current Balance"
+      />
     </main>
   )
 }
