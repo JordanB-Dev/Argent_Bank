e6c5142cb66f759818d09c95664f7593c766b2c3
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-03-11 07:29:21 +0100

    Feature => map

--- Front/src/pages/Home/index.jsx
+++ Front/src/pages/Home/index.jsx
@@ -5,31 +5,42 @@ import iconMoney from '../../assets/icon-money.png'
 import iconSecurity from '../../assets/icon-security.png'
 
 const Home = () => {
+  const features = [
+    {
+      src: iconChat,
+      title: 'You are our #1 priority',
+      alt: 'Chat Icon',
+      desc: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+      src: iconMoney,
+      alt: 'Chat Icon',
+      title: 'More savings means higher rates',
+      desc: 'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+      src: iconSecurity,
+      alt: 'Chat Icon',
+      title: 'Security you can trust',
+      desc: 'We use top of the line encryption to make sure your data and money is always safe.',
+    },
+  ]
   return (
     <main>
       <Hero />
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        <Feature
-          src={iconChat}
-          alt="Chat Icon"
-          title="You are our #1 priority"
-          desc="Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes."
-        />
-        <Feature
-          src={iconMoney}
-          alt="Chat Icon"
-          title="More savings means higher rates"
-          desc="The more you save with us, the higher your interest rate will be!"
-        />
-        <Feature
-          src={iconSecurity}
-          alt="Chat Icon"
-          title="Security you can trust"
-          desc="We use top of the line encryption to make sure your data and money
-          is always safe."
-        />
+        {features.map((feature, index) => {
+          return (
+            <Feature
+              key={index}
+              alt={feature.alt}
+              src={feature.src}
+              title={feature.title}
+              desc={feature.desc}
+            />
+          )
+        })}
       </section>
     </main>
   )
